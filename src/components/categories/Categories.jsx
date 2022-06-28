09e2eff727b6959143a896d941a3083fcda9f50f
Игорь Михайлович Панасеня <panasenya1995@mail.ru>
2022-06-28 17:01:29 +0300

    Initial commit

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -6,6 +6,7 @@ import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import './categories.scss';
 
 const Categories = () => {
     const [categories, setCategories] = useState([])
@@ -19,23 +20,32 @@ const Categories = () => {
 
 
     // Отслеживание ширины экрана для слайдера КАТЕГОРИЙ
-    const [widthScreen, setWidthScreen] = useState(window.screen.width)
-    const getWidthScreen = () => {
-        window.addEventListener(`resize`, event => {
-            setWidthScreen(window.screen.width);
-            console.log(widthScreen)
-        }, false);
-        return widthScreen
-    }
-    getWidthScreen()
+    // const [widthScreen, setWidthScreen] = useState(window.screen.width)
+    // const getWidthScreen = () => {
+    //     window.addEventListener(`resize`, event => {
+    //         setWidthScreen(window.screen.width);
+    //         console.log(widthScreen)
+    //     }, false);
+    //     return widthScreen
+    // }
+    // getWidthScreen()
 
     return (
         <section className='container'>
             <Swiper
                     modules={[Pagination]}
-                    spaceBetween={20}
-                    slidesPerView={widthScreen > '1400' ? '3' : '1'}
+                    spaceBetween={140}
                     pagination={{ clickable: true }}
+                    breakpoints={{
+                        0 : {
+                            width: 0,
+                            slidesPerView: 1,
+                        },
+                        1000 : {
+                            width: 1000,
+                            slidesPerView: 3,
+                        }
+                    }}
             >
                     {categories.map(catItem =>
                         <SwiperSlide key={catItem.id} >
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './footer.scss';
+import Logo from '../../img/footer/logo-black.png';
 
 const Footer = () => {
     return (
         <footer id='contact'>
             <div className="footerInfo">
-                <img src='/img/footer/logo-black.png' alt="cityman"/>
+                <img src={Logo} alt="cityman"/>
                     <p>gywpo@mailto.plus</p>
                     <p>+1-705-750-7827</p>
                     <p>523 Garfield St, Lander<br/>WY 82520, USA</p>
